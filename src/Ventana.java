c7eb6f7a443787b7c22bc6c395f15db9697b75eb
Andres <andresfelipeasprillaurrutia@gmail.com>
2023-05-22 18:40:25 -0500

    Version 3.0 (Labels actualizacion)

--- src/Ventana.java
+++ src/Ventana.java
@@ -2,93 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 public class Ventana extends JFrame {
-    private JPanel panel1;
-    private JPanel Pinicio;
-    private JButton Bfacil;
-    private JButton Bmedio;
-    private JButton Bdificil;
-    private JButton Baspecto;
-    private JPanel Pmenus;
-    private JPanel Popciones;
-    private JButton Bvolver;
-    private JPanel Pmedio;
-    private JButton a1Button;
-    private JButton a2Button;
-    private JButton a3Button;
-    private JButton a4Button;
-    private JButton a7Button;
-    private JLabel Ltimer;
-    private JPanel Paspectos;
-    private JButton a8Button;
-    private JButton a6Button;
-    private JButton a5Button;
-    private JPanel Pdificil;
-    private JPanel Pfacil;
-    private JButton a12Button;
-    private JButton Breiniciar;
-    private JButton Bempezar;
-    private JPanel PopcionesJuego;
-    private JButton BequiposDeFutbol;
-    private JButton Bmemes;
-    private JButton Balbumes;
-    private JPanel Pjuego;
-    private JLabel L1;
-    private JLabel L2;
-    private JLabel L3;
-    private JLabel L4;
-    private JLabel L5;
-    private JLabel L6;
-    private JLabel L7;
-    private JLabel L8;
-    private JLabel L9;
-    private JLabel L10;
-    private JLabel L11;
-    private JLabel L12;
-    private JLabel L13;
-    private JLabel L14;
-    private JLabel L15;
-    private JLabel L16;
-    private JButton a9Button;
-    private JButton a10Button;
-    private JButton a11Button;
+    private JPanel panel1, Pinicio, Pmenus, Popciones, Pjuego, Pfacil, Pmedio, Pdificil, Paspectos, PopcionesJuego;
+    private JButton Bfacil, Bmedio, Bdificil, Baspecto, Bvolver, Bequipos, Bmemes, Balbumes, Breiniciar, Bempezar;
+    private JLabel L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, L11, L12, L13, L14, L15, L16, Ltimer;
     private Timer Ctimer;
-    private int horas = 0;
-    private int minutos= 0;
-    private int segundos;
-    private Color colorAzul = new Color(67,80,163);
-    //////////////////////////////////////////////////////////////////////// Imagenes de memes
-    private ImageIcon M1 = new ImageIcon("\\Imagenes\\Memes\\1.jpg");
-    private ImageIcon M2 = new ImageIcon("\\Imagenes\\Memes\\2.jpg");
-    private ImageIcon M3 = new ImageIcon("\\Imagenes\\Memes\\3.jpg");
-    private ImageIcon M4 = new ImageIcon("\\Imagenes\\Memes\\4.jpg");
-    private ImageIcon M5 = new ImageIcon("\\Imagenes\\Memes\\5.jpg");
-    private ImageIcon M6 = new ImageIcon("\\Imagenes\\Memes\\6.jpg");
-    private ImageIcon M7 = new ImageIcon("\\Imagenes\\Memes\\7.jpg");
-    private ImageIcon M8 = new ImageIcon("\\Imagenes\\Memes\\8.jpg");
-////////////////////////////////////////////////////////////////////////////////////////////// Imagenes de equipos de futbol
-    private ImageIcon E1 = new ImageIcon("\\src\\Imagenes\\Equipos de futbol\\1.jpg");
-    private ImageIcon E2 = new ImageIcon("\\src\\Imagenes\\Equipos de futbol\\2.jpg");
-    private ImageIcon E3 = new ImageIcon("\\src\\Imagenes\\Equipos de futbol\\3.jpg");
-    private ImageIcon E4 = new ImageIcon("\\src\\Imagenes\\Equipos de futbol\\4.jpg");
-    private ImageIcon E5 = new ImageIcon("\\src\\Imagenes\\Equipos de futbol\\5.jpg");
-    private ImageIcon E6 = new ImageIcon("\\src\\Imagenes\\Equipos de futbol\\6.jpg");
-    private ImageIcon E7 = new ImageIcon("\\src\\Imagenes\\Equipos de futbol\\7.jpg");
-    private ImageIcon E8 = new ImageIcon("\\src\\Imagenes\\Equipos de futbol\\8.jpg");
-    ////////////////////////////////////////////////////////////////////////////////////////////// Imagenes de albumes
-    private ImageIcon A1 = new ImageIcon("\\src\\Imagenes\\Albumes\\1.jpg");
-    private ImageIcon A2 = new ImageIcon("\\src\\Imagenes\\Albumes\\2.jpg");
-    private ImageIcon A3 = new ImageIcon("\\src\\Imagenes\\Albumes\\3.jpg");
-    private ImageIcon A4 = new ImageIcon("\\src\\Imagenes\\Albumes\\4.jpg");
-    private ImageIcon A5 = new ImageIcon("\\src\\Imagenes\\Albumes\\5.jpg");
-    private ImageIcon A6 = new ImageIcon("\\src\\Imagenes\\Albumes\\6.jpg");
-    private ImageIcon A7 = new ImageIcon("\\src\\Imagenes\\Albumes\\7.jpg");
-    private ImageIcon A8 = new ImageIcon("\\src\\Imagenes\\Albumes\\8.jpg");
-
-    private JButton[] botones = {a1Button,a2Button,a3Button,a4Button,a5Button,a6Button,a7Button,a8Button,a9Button,a10Button,a11Button,a12Button}; //Adentro van las imagenes
+    private int horas = 0, minutos = 0, segundos = 0;
+    private ArrayList<Integer>images = new ArrayList<>();
+    private String cartas = "Memes";
+    private JLabel[] labels = {L1,L2,L3,L4,L5,L6,L7,L8,L9,L10,L11,L12,L13,L14,L15,L16}; //Adentro van las imagenes
     int[] vector = new int[16];
 
     public void ActualizarLtimer() //Actualiza el label del cronometro
@@ -99,12 +27,25 @@ public class Ventana extends JFrame {
         public Ventana()
         {
         super("Memory Game");
+        for(int i = 0; i < 2; i++)
+        {
+           images.add(1);
+           images.add(2);
+           images.add(3);
+           images.add(4);
+           images.add(5);
+           images.add(6);
+           images.add(7);
+           images.add(8);
+        }
+        System.out.println(String.valueOf(images));
         setContentPane(panel1);
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         Bfacil.setFocusPainted(false);
         Bmedio.setFocusPainted(false);
         Bdificil.setFocusPainted(false);
         Pmenus.setVisible(false);
+        Color colorAzul = new Color(67, 80, 163);
         Pmenus.setBackground(colorAzul);
         Pfacil.setBackground(colorAzul);
         Pmedio.setBackground(colorAzul);
@@ -113,6 +54,7 @@ public class Ventana extends JFrame {
         Popciones.setBackground(colorAzul);
         PopcionesJuego.setBackground(colorAzul);
         Paspectos.setBackground(colorAzul);
+        Pjuego.setBackground(colorAzul);
 
         Ctimer=new Timer(1000, e -> { //Uso de Timer para el funcionamiento del cronometro
             segundos++;
@@ -142,12 +84,6 @@ public class Ventana extends JFrame {
             {
                 Pinicio.setVisible(true);
                 Pmenus.setVisible(false);
-                Paspectos.setVisible(false);
-                Popciones.setVisible(false);
-                PopcionesJuego.setVisible(false);
-                Pfacil.setVisible(false);
-                Pmedio.setVisible(false);
-                Pdificil.setVisible(false);
 
                 if(Ctimer.isRunning())
                 {
@@ -194,11 +130,7 @@ public class Ventana extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                Pinicio.setVisible(false);
-                Pmenus.setVisible(true);
-                Paspectos.setVisible(false);
-                Popciones.setVisible(true);
-                PopcionesJuego.setVisible(true);
+                juego();
                 Pfacil.setVisible(true);
                 Pmedio.setVisible(false);
                 Pdificil.setVisible(false);
@@ -209,12 +141,8 @@ public class Ventana extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                Pinicio.setVisible(false);
-                Pmenus.setVisible(true);
-                Paspectos.setVisible(false);
-                Popciones.setVisible(true);
-                PopcionesJuego.setVisible(true);
-                Pfacil.setVisible(false);
+                juego();
+                Pfacil.setVisible(true);
                 Pmedio.setVisible(true);
                 Pdificil.setVisible(false);
             }
@@ -225,13 +153,9 @@ public class Ventana extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                Pinicio.setVisible(false);
-                Pmenus.setVisible(true);
-                Paspectos.setVisible(false);
-                Popciones.setVisible(true);
-                PopcionesJuego.setVisible(true);
-                Pfacil.setVisible(false);
-                Pmedio.setVisible(false);
+                juego();
+                Pfacil.setVisible(true);
+                Pmedio.setVisible(true);
                 Pdificil.setVisible(true);
             }
         });
@@ -242,79 +166,83 @@ public class Ventana extends JFrame {
             public void actionPerformed(ActionEvent e)
             {
                 Pinicio.setVisible(false);
-                Pmenus.setVisible(true);
-                Paspectos.setVisible(true);
-                Popciones.setVisible(true);
                 PopcionesJuego.setVisible(false);
                 Pfacil.setVisible(false);
                 Pmedio.setVisible(false);
                 Pdificil.setVisible(false);
+                Pmenus.setVisible(true);
+                Paspectos.setVisible(true);
+
             }
         });
 
-//////////////////////////////////////////////// PRUEBA DE VECTORES
-        BequiposDeFutbol.addActionListener(new ActionListener()
+//////////////////////////////////////////////// Definen el set de cartas
+        Bequipos.addActionListener(new ActionListener()
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                //Aqui iria: public void displayImages() {
-                //        for(int i=0; i < vector.length; i++) {
-                //            try {
-                //                ImageIcon icon = new ImageIcon(getClass().getResource("/Imagenes/Equipos de futbol" + vector[i] + ".jpg"));
-                //                botones[i].setIcon(icon);
-                //            } catch (Exception e) {
-                //                e.printStackTrace();
-                //            }
-                //        }
-                //    }
+                cartas = "Equipos";
             }
         });
 
-        Bmemes.addActionListener(new ActionListener() {
+        Bmemes.addActionListener(new ActionListener()
+        {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                //Aqui iria: public void displayImages() {
-                //        for(int i=0; i < vector.length; i++) {
-                //            try {
-                //                ImageIcon icon = new ImageIcon(getClass().getResource("/Imagenes/Memes" + vector[i] + ".jpg"));
-                //                botones[i].setIcon(icon);
-                //            } catch (Exception e) {
-                //                e.printStackTrace();
-                //            }
-                //        }
-                //    }
+                cartas = "Memes";
+            }
+        });
+        Balbumes.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                cartas = "Albumes";
+            }
+        });
+        
+        L1.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+              displayImages();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
             }
         });
     }
-
-    public void generarVectorFacil() {
-        // Inicializa el vector con valor 0
-        Arrays.fill(vector, 0);
-        Random random = new Random();
-
-        // Agrega los numeros entre 1 y 8 en dos posiciones diferentes de las 16 disponibles en el arreglo
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 0; j < 2; j++) {
-                int posicion;
-                // El ciclo se ejecuta mientras haya un valor distinto de cero en la posición del arreglo
-                // Esto indica que debe encontrar una posición en cero para asignar el valor de i
-                do {
-                    posicion = random.nextInt(4);
-                } while (vector[posicion] != 0);
-                vector[posicion] = i;
-            }
-        }
-
-        System.out.println("El vector resultante es: " + Arrays.toString(vector));
+    public void juego()
+    {
+        Paspectos.setVisible(false);
+        Pinicio.setVisible(false);
+        Pmenus.setVisible(true);
+        PopcionesJuego.setVisible(true);
     }
-
     public void displayImages() {
         for(int i=0; i < vector.length; i++) {
             try {
-                ImageIcon icon = new ImageIcon(getClass().getResource("/imgs/landscape" + vector[i] + ".jpg"));
-                botones[i].setIcon(icon);
+                ImageIcon icon = new ImageIcon(getClass().getResource("/Imagenes/" +
+                        cartas + "/" + String.valueOf(images.get(i)) + ".jpg"));
+                labels[i].setIcon(icon);
             } catch (Exception e) {
                 e.printStackTrace();
             }
